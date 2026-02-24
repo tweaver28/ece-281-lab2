fc65eabe8701dcec5504612a61bf3ecb2df35b47
tweaver28 <C28Terry.Weaver@afacademy.af.edu>
2026-02-23 19:54:54 -0700

    New files

---
+++ src/sevenseg_decoder_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/23/2026 07:49:35 PM
+// Design Name: 
+// Module Name: sevenseg_decoder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sevenseg_decoder_tb(
+    input [3:0] i_Hex,
+    input [6:0] o_seg_n
+    );
+endmodule
